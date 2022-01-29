925e51db0b17139775cc844398e65e0ccd5d3e64
braxo256 <ericksonbravo.a@gmail.com>
2022-01-29 11:45:21 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -4,21 +4,31 @@
 	ejecucion.call()
 */
 def call(){
-    stage("Paso 1: Build && Test"){
+    env.STAGE = "Paso 1: Build  Test"
+    stage("$env.STAGE "){
+    // stage("Paso 1: Build && Test"){
         sh "gradle clean build"
     }
-    stage("Paso 2: Sonar - Análisis Estático"){
+
+    env.STAGE = "Paso 2: Sonar - Análisis Estático"
+    stage("$env.STAGE "){
+    // stage("Paso 2: Sonar - Análisis Estático"){
         sh "echo 'Análisis Estático!'"
         withSonarQubeEnv('sonarqube') {
              sh 'chmod +x gradlew'
             sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
-    stage("Paso 3: Curl Springboot Gradle sleep 40"){
+    env.STAGE = "Paso 3: Curl Springboot Gradle sleep 40"
+    stage("$env.STAGE "){
+    // stage("Paso 3: Curl Springboot Gradle sleep 40"){
         sh "gradle bootRun&"
         sh "sleep 40 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
-    stage("Paso 4: Subir Nexus"){
+
+     env.STAGE = "Paso 4: Subir Nexus"
+    stage("$env.STAGE "){
+    // stage("Paso 4: Subir Nexus"){
         nexusPublisher nexusInstanceId: 'nexus',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
@@ -38,14 +48,23 @@ def call(){
             ]
         ]
     }
-    stage("Paso 5: Descargar Nexus"){
+
+    env.STAGE = "Paso 5: Descargar Nexus"
+    stage("$env.STAGE "){
+    // stage("Paso 5: Descargar Nexus"){
         sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexucito:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
-    stage("Paso 6: Levantar Artefacto Jar"){
+
+    env.STAGE = "Paso 6: Levantar Artefacto Jar"
+    stage("$env.STAGE "){
+    // stage("Paso 6: Levantar Artefacto Jar"){
         // sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
         sh 'java -jar DevOpsUsach2020-0.0.1.jar &'
     }
-    stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
+
+    env.STAGE = "Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "
+    stage("$env.STAGE "){
+    // stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
